c61fcab38c46198d22b89119c8c991405596e45a
Julio Cesar S. Cunha <jcesarsc@gmail.com>
2022-10-18 12:33:30 -0300

    changes from the lesson 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-import "./css/index.css"
+import "./css/index.css";
+
+const ccBgColor01 = document.querySelector("div.cc-bg > svg > g > g:nth-child(1) > path");
+const ccBgColor02 = document.querySelector("div.cc-bg > svg > g > g:nth-child(2) > path");
+const ccBgColor03 = document.querySelector("div.cc-bg > svg > g > g:nth-child(3) > path");
+const ccBgLogo01 = document.querySelector(".cc-logo > span:nth-child(2) > img");
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436d99", "#2d57f2"],
+    mastercard: ["#df6f29", "#c69347"],
+    american: ["#0077A6", "#FFFFFF"],
+    default: ["black", "gray"],
+  };
+
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor02.setAttribute("fill", colors[type][1]);
+  ccBgLogo01.setAttribute("src", `cc-${type}.svg`);
+}
+
+// setCardType("american");
+globalThis.setCardType = setCardType;
