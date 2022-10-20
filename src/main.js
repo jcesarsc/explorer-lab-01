df4a97d01070ab0a36779b8180814d2afb6c3fc7
Julio Cesar S. Cunha <jcesarsc@gmail.com>
2022-10-20 10:41:37 -0300

    changes from the lesson 2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import "./css/index.css";
+import IMask from "imask";
 
 const ccBgColor01 = document.querySelector("div.cc-bg > svg > g > g:nth-child(1) > path");
 const ccBgColor02 = document.querySelector("div.cc-bg > svg > g > g:nth-child(2) > path");
-const ccBgColor03 = document.querySelector("div.cc-bg > svg > g > g:nth-child(3) > path");
 const ccBgLogo01 = document.querySelector(".cc-logo > span:nth-child(2) > img");
 
 function setCardType(type) {
@@ -20,3 +20,67 @@ function setCardType(type) {
 
 // setCardType("american");
 globalThis.setCardType = setCardType;
+
+const securityCode = document.querySelector("#security-code");
+const secCodePattern = {
+  mask: "0000",
+};
+const securityCodeMasked = IMask(securityCode, secCodePattern);
+
+const expirationDate = document.querySelector("#expiration-date");
+const expDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+};
+const expirationDateMasked = IMask(expirationDate, expDatePattern);
+
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000-0000-0000-0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000-0000-0000-0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000-0000-0000-0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex);
+    });
+    // console.log(foundMask);
+
+    return foundMask;
+  },
+};
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+const addButton = document.querySelector("#add-card");
+addButton.addEventListener("click", () => {
+  console.log("vc clicou");
+});
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
